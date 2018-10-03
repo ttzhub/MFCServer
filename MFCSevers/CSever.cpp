6bd234cf3efb24bedffce9f93d1fa40aa7469834
ttzhub <tangtongzettz@163.com>
2018-10-03 19:57:59 +0800

    CSever添加必要的虚函数

--- MFCSevers/CSever.cpp
+++ MFCSevers/CSever.cpp
@@ -10,3 +10,27 @@ CSever::CSever()
 CSever::~CSever()
 {
 }
+
+
+void CSever::OnAccept(int nErrorCode)
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	CSocket::OnAccept(nErrorCode);
+}
+
+
+void CSever::OnClose(int nErrorCode)
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	CSocket::OnClose(nErrorCode);
+}
+
+
+void CSever::OnReceive(int nErrorCode)
+{
+	// TODO: 在此添加专用代码和/或调用基类
+
+	CSocket::OnReceive(nErrorCode);
+}
--- MFCSevers/CSever.h
+++ MFCSevers/CSever.h
@@ -4,5 +4,8 @@ class CSever : public CSocket
 public:
 	CSever();
 	~CSever();
+	virtual void OnAccept(int nErrorCode);
+	virtual void OnClose(int nErrorCode);
+	virtual void OnReceive(int nErrorCode);
 };
 
