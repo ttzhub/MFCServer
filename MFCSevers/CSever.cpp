399bebedac58d521225458d47bebb2cb0b7ce1c3
ttzhub <tangtongzettz@163.com>
2018-10-03 20:46:05 +0800

    编辑CSever.cpp 之后

--- MFCSevers/CSever.cpp
+++ MFCSevers/CSever.cpp
@@ -15,7 +15,7 @@ CSever::~CSever()
 void CSever::OnAccept(int nErrorCode)
 {
 	// TODO: 在此添加专用代码和/或调用基类
-
+	m_SeverDlg->AddClient();                                  //添加上线用户
 	CSocket::OnAccept(nErrorCode);
 }
 
@@ -23,7 +23,7 @@ void CSever::OnAccept(int nErrorCode)
 void CSever::OnClose(int nErrorCode)
 {
 	// TODO: 在此添加专用代码和/或调用基类
-
+	m_SeverDlg->RemoveClient(this);                   // 删除下线用户
 	CSocket::OnClose(nErrorCode);
 }
 
@@ -31,6 +31,6 @@ void CSever::OnClose(int nErrorCode)
 void CSever::OnReceive(int nErrorCode)
 {
 	// TODO: 在此添加专用代码和/或调用基类
-
+	m_SeverDlg->RecvData(this);                           // 接收数据
 	CSocket::OnReceive(nErrorCode);
 }
--- MFCSevers/CSever.h
+++ MFCSevers/CSever.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "MFCSeversDlg.h"			// 
+#include "MFCSeversDlg.h"			// 主对话框头文件
 
 class CMFCSeversDlg;
 class CSever : public CSocket
@@ -10,5 +10,7 @@ public:
 	virtual void OnAccept(int nErrorCode);
 	virtual void OnClose(int nErrorCode);
 	virtual void OnReceive(int nErrorCode);
+	// 主对话框指针
+	CMFCSeversDlg * m_SeverDlg;
 };
 
